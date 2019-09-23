680db25c146cac3d1a2584cc864030574d9f99d9
Kuldeepkkd <51772095+Kuldeepkkd@users.noreply.github.com>
2019-09-23 08:55:45 +0530

    7 pattern programs added

---
+++ Pattern3.java
@@ -0,0 +1,20 @@
+/*
+55555
+44444
+33333
+22222
+11111
+*/
+public class Pattern3 
+{
+public static void main(String args[])
+{
+    int i,j;
+    for(i=5;i>=1;i--)
+    {
+            for(j=1;j<=5;j++)
+            System.out.print(i);
+            System.out.println("");
+    }
+}
+}
